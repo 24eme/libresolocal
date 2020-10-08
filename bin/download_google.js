2dd6ac9fd058d5e8812852244acd5c8bdbae5e99
Vincent LAURENT <vince.laurent@gmail.com>
2020-10-08 23:05:50 +0200

    Suppression de la popup google

--- bin/download_google.js
+++ bin/download_google.js
@@ -3,10 +3,21 @@ var url = process.argv[2];
 var imagepath = process.argv[3];
 const nightmare = Nightmare({ show: true })
 
+Nightmare.action('removePopup', function(done) {
+  this.evaluate_now(function() {
+      document.querySelector('iframe').remove();
+      document.querySelector('.widget-consent.widget-consent-fullscreen').remove();
+  }, done)
+})
+
 nightmare
   .goto(url)
+  .removePopup()
   .screenshot(imagepath)
-  .evaluate(() => (document.documentElement.innerHTML))
+  .click('.allxGeDnJMl__button.allxGeDnJMl__button-text')
+  .wait('.ozj7Vb3wnYq__title.gm2-headline-6')
+  .evaluate(() => (
+      document.documentElement.innerHTML))
   .end()
   .then(console.log)
   .catch(error => {
