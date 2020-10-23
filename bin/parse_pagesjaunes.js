a9821303563dd3265ca04c3df8b77218d6ac20cd
Vincent LAURENT <vince.laurent@gmail.com>
2020-10-24 01:28:01 +0200

    Adaptation modification html de pages jaunes

--- bin/parse_pagesjaunes.js
+++ bin/parse_pagesjaunes.js
@@ -5,12 +5,12 @@ const $ = cheerio.load(fs.readFileSync(fileHTML));
 
 var plateforme = "pagesjaunes"
 var nom = $('#teaser-header .denom h1').text();
-var adresse = $('#teaser-footer .zone-coordonnees .address').text();
+var adresse = $('#teaser-footer .zone-coordonnees .address span.noTrad').text();
 var phone = $('#teaser-footer .zone-coordonnees .coord-numero').text();
 var site = $('#teaser-footer .zone-coordonnees .lvs-container .pj-link.pj-lb .value').text();
 var note = $('#teaser-header .fd-note strong').text();
 var nbNotes = $('#teaser-header .teaser-nombre-avis').text().replace("Voir les ", "").replace(" avis", "");
-var categorie = $('#zone-info .prestations ul.list-v li span').text();
+var categories = $('#teaser-header .zone-activites .activite');
 
 var baseLigne = plateforme + ";" + nom;
 
@@ -18,9 +18,13 @@ console.log(baseLigne + ";nom;" + nom);
 console.log(baseLigne + ";adresse;" + adresse);
 console.log(baseLigne + ";telephone;" + phone);
 console.log(baseLigne + ";site;" + site);
-console.log(baseLigne + ";note;" + note.replace(",", ".") + "/5");
+if(note) {
+    console.log(baseLigne + ";note;" + note.replace(",", ".") + "/5");
+}
 console.log(baseLigne + ";nombre_avis;" + nbNotes);
-console.log(baseLigne + ";categorie;" + categorie);
+categories.each(function() {
+    console.log(baseLigne + ";categorie;" + $(this).text());
+});
 $('#infos-horaires ul.liste-horaires-principaux > li').each(function() {
     var jour = $(this).find('p').text().toLowerCase();
     var horaires = []
