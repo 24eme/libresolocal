110750c237f83a7129fced2a4bc979c590714017
Vincent LAURENT <vince.laurent@gmail.com>
2020-11-01 23:46:39 +0100

    Mise en forme de la liste des pages

--- view/commerce.html.php
+++ view/commerce.html.php
@@ -4,7 +4,7 @@
     <!-- Required meta tags -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-
+    <title><?php echo $commerce->getName(); ?> - Libre Solocal</title>
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
   </head>
@@ -13,63 +13,85 @@
         <h2 class="mt-4 mb-3"><?php echo $commerce->getName(); ?> <small class="text-secondary"><?php echo $commerce->getAddress(); ?></small></h2>
         <ul class="nav nav-tabs" role="tablist">
             <li class="nav-item">
-                <a class="nav-link active" id="informations_nav" data-toggle="tab" href="#informations_tab" role="tab" aria-controls="informations_tab" aria-selected="true">Informations <span class="badge badge-dark"><?php echo count($commerce->getPages()) ?></span></a>
+                <a class="nav-link active" id="informations_nav" data-toggle="tab" href="#informations_tab" role="tab" aria-controls="informations_tab" aria-selected="true">Informations <span class="badge badge-primary badge-pill"><?php echo count($commerce->getPages()) ?></span></a>
             </li>
             <li class="nav-item">
-                <a class="nav-link" id="reviews_nav" data-toggle="tab" href="#reviews_tab" role="tab" aria-controls="reviews_tab" aria-selected="false">Avis <span class="badge badge-dark"><?php echo count($commerce->getReviews()) ?></span></a>
+                <a class="nav-link" id="reviews_nav" data-toggle="tab" href="#reviews_tab" role="tab" aria-controls="reviews_tab" aria-selected="false">Avis <span class="badge badge-primary badge-pill"><?php echo count($commerce->getReviews()) ?></span></a>
             </li>
             <li class="nav-item">
-                <a class="nav-link" id="images_nav" data-toggle="tab" href="#images_tab" role="tab" aria-controls="images_tab" aria-selected="false">Images <span class="badge badge-dark"></span></a>
+                <a class="nav-link" id="images_nav" data-toggle="tab" href="#images_tab" role="tab" aria-controls="images_tab" aria-selected="false">Images <span class="badge badge-primary badge-pill"></span></a>
             </li>
         </ul>
-        <div class="tab-content">
+        <div class="tab-content bg-white border-left border-right border-bottom p-3">
             <div class="tab-pane show active" id="informations_tab" role="tabpanel" aria-labelledby="informations_nav">
+                <ul class="list-group list-group-flush">
                 <?php foreach($commerce->getPages() as $page): ?>
-                    <div class="card mt-2">
-                      <div class="row no-gutters">
-                        <div class="col-md-4">
-                          <img src="data:image/png;base64,<?php echo base64_encode(file_get_contents($page->getImageFile())); ?>" class="card-img" alt="">
-                        </div>
-                        <div class="col-md-4">
-                          <div class="card-body">
-                            <h5 class="card-title"><?php echo $page->getPlateform() ?></h5>
-                            <p class="card-text"><?php echo $page->getName() ?></p>
-                            <p class="card-text"><?php echo $page->getAddress() ?></p>
-                            <p class="card-text"><a href=""><?php echo $page->getPhone() ?></a> <br />
-                            <a href=""><?php echo $page->getWebsite() ?></a></p>
-                            <p class="card-text"><?php echo $page->getScore() ?> <?php echo ($page->getReviewsCount()) ? $page->getReviewsCount() : 'aucun' ?> avis</p>
-                          </div>
-                        </div>
-                        <div class="col-md-4">
-                          <div class="card-body">
-                            <h5 class="card-title">&nbsp;</h5>
-                            <p class="card-text">
-                                Lundi&nbsp;:&nbsp;<?php echo $page->getHour('lundi') ?><br />
-                                Mardi&nbsp;:&nbsp;<?php echo $page->getHour('mardi') ?><br />
-                                Mercredi&nbsp;:&nbsp;<?php echo $page->getHour('mercredi') ?><br />
-                                Jeudi&nbsp;:&nbsp;<?php echo $page->getHour('jeudi') ?><br />
-                                Vendredi&nbsp;:&nbsp;<?php echo $page->getHour('vendredi') ?><br />
-                                Samedi&nbsp;:&nbsp;<?php echo $page->getHour('samedi') ?><br />
-                                Dimanche&nbsp;:&nbsp;<?php echo $page->getHour('dimanche') ?>
-                            </p>
-                          </div>
-                        </div>
-                      </div>
-                    </div>
+                        <li class="list-group-item">
+                            <div class="row">
+                                <div class="col-md-4">
+                                  <img src="data:image/png;base64,<?php echo base64_encode(file_get_contents($page->getImageFile())); ?>" class="card-img" alt="">
+                                </div>
+                                <div class="col-md-4">
+                                    <h5 class="mb-3 text-secondary"><?php echo $page->getPlateform() ?></h5>
+                                    <h5><?php echo $page->getName() ?></h5>
+                                    <p><?php echo str_replace(",", "<br />", $page->getAddress()); ?></p>
+                                    <p><?php echo $page->getPhone() ?><br />
+                                    <a href=""><?php echo $page->getWebsite() ?></a></p>
+                                </div>
+                                <div class="col-md-4">
+                                    <table class="table table-bordered table-striped table-sm small mb-0">
+                                        <tr>
+                                            <th class="text-center" colspan="2">Ouvertures</th>
+                                        </tr>
+                                        <tr>
+                                            <th>Lundi</th>
+                                            <td><?php echo $page->getHour('lundi') ?></td>
+                                        </tr>
+                                        <tr>
+                                            <th>Mardi</th>
+                                            <td><?php echo $page->getHour('mardi') ?></td>
+                                        </tr>
+                                        <tr>
+                                            <th>Mercredi</th>
+                                            <td><?php echo $page->getHour('mercredi') ?></td>
+                                        </tr>
+                                        <tr>
+                                            <th>Jeudi</th>
+                                            <td><?php echo $page->getHour('jeudi') ?></td>
+                                        </tr>
+                                        <tr>
+                                            <th>Vendredi</th>
+                                            <td><?php echo $page->getHour('vendredi') ?></td>
+                                        </tr>
+                                        <tr>
+                                            <th>Samedi</th>
+                                            <td><?php echo $page->getHour('samedi') ?></td>
+                                        </tr>
+                                        <tr>
+                                            <th>Dimanche</th>
+                                            <td><?php echo $page->getHour('dimanche') ?></td>
+                                        </tr>
+                                    </table>
+                                </div>
+                            </div>
+                        </li>
                 <?php endforeach; ?>
+                </ul>
             </div>
-            <div class="tab-pane pt-3" id="reviews_tab" role="tabpanel" aria-labelledby="reviews_nav">
+            <div class="tab-pane" id="reviews_tab" role="tabpanel" aria-labelledby="reviews_nav">
                 <div class="row">
                     <div class="col">
-                        <div class="list-group">
+                        <ul class="list-group list-group-flush">
                         <?php foreach($commerce->getReviews() as $review): ?>
-                            <li class="list-group-item ">
+                            <li class="list-group-item">
                                 <div class="row">
                                     <div class="col-10">
                                         <?php if($review->getContent()): ?>
-                                        <p class="mb-2"><?php echo $review->getContent(); ?></p>
+                                            <p class="mb-2"><?php echo $review->getContent(); ?></p>
+                                        <?php else: ?>
+                                            <p class="mb-2 text-muted"><em>Aucun message</em></p>
                                         <?php endif; ?>
-                                        <small><?php echo $review->getAuthor(); ?> sur <strong><?php echo $review->getPlateform(); ?></strong><?php if($review->getScore()): ?> a mis la note de <strong><?php echo $review->getScore() ?></strong><?php endif; ?></small>
+                                        <span class="blockquote-footer"><?php echo $review->getAuthor(); ?> sur <cite><?php echo $review->getPlateform(); ?></cite><?php if($review->getScore()): ?> qui a mis la note de <strong><?php echo $review->getScore() ?></strong><?php endif; ?></span>
                                     </div>
                                     <div class="col-2 text-right">
                                         <small class="text-muted"><?php echo $review->getDate()->format("F Y"); ?></small>
@@ -77,14 +99,14 @@
                                 </div>
                             </li>
                         <?php endforeach; ?>
-                        </div>
+                        </ul>
                     </div>
                     <div class="col-3">
                         <?php foreach($commerce->getReviewsFacets() as $facetName => $facets): ?>
-                        <h5><?php echo $facetName ?></h5>
+                        <h6 class="text-dark"><?php echo $facetName ?></h6>
                         <div class="list-group mb-3">
                             <?php foreach($facets as $name => $value): ?>
-                                <a href="" class="list-group-item list-group-item-action d-flex justify-content-between align-items-center"><?php echo $name ?> <span class="badge badge-primary badge-pill"><?php echo $value ?></span></a>
+                                <a href="" class="list-group-item list-group-item-action d-flex justify-content-between align-items-center"><small><?php echo $name ?></small> <span class="badge badge-secondary badge-pill"><?php echo $value ?></span></a>
                             <?php endforeach; ?>
                         </div>
                         <?php endforeach; ?>
